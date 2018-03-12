b05abf2c79ae8ac042a948f8957dff5f32cea910
Nicole Eidi <nicoleeidi1@gmail.com>
2018-03-12 18:39:12 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -4,10 +4,11 @@ class App < Sinatra::Base
   get '/' do
     "Hello, World!"
   end
-  get 'name' do 
-  end 
-  get 'hometown' do 
-  end 
-  get 'favorite-song' do 
-  end 
+  get 'name' do
+    "My name is __"
+  end
+  get 'hometown' do
+  end
+  get 'favorite-song' do
+  end
 end
