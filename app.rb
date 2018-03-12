42a61860be7ec3be2cff03cf46d8ab07a7732c82
Nicole Eidi <nicoleeidi1@gmail.com>
2018-03-12 18:38:54 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,7 +1,13 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  get '/' do 
+  get '/' do
     "Hello, World!"
+  end
+  get 'name' do 
+  end 
+  get 'hometown' do 
+  end 
+  get 'favorite-song' do 
   end 
 end
